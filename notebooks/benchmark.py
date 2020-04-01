d4377169dd4195e791c9c26cf98e84b12e0164d7
jjagusch <jan@instaffo.de>
2020-04-01 18:28:38 +0200

    #20 add benchmarking notebook

---
+++ notebooks/benchmark.py
@@ -0,0 +1,58 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python (connect-x)
+#     language: python
+#     name: connect-x
+# ---
+
+# # Benchmark Environment
+
+# This notebook provides a benchmark environment, where you can compare two versions of your agents and let them compete against each other.
+#
+# **Requirements:**
+# * You have two standalone submissions in your `/benchmark` directory (`agent_1_standalone.py` and `agent_2_standalone.py`)
+# * You have [Docker](https://www.docker.com/) and [Docker Compose](https://docs.docker.com/compose/) installed on your machine.
+#
+# **Getting Started:**
+# * Navigate to `/benchmark`
+# * Execute `docker-compose up --build`
+# * Then start this notebook.
+#
+# **Further information:**
+# * You can also let your algorithm compete against `random` or `negamax` agent.
+# * You will need [Jupytext](https://github.com/mwouts/jupytext) to convert this `.py` light script into a Jupyter notebook.
+
+from kaggle_environments import make
+import requests
+
+env = make("connectx")
+_ = env.reset()
+
+
+def act(url, observation, configuration):
+    """
+    Sends a post request to one of the two agents.
+    """
+    data = {"observation": observation, "configuration": configuration}
+    response = requests.post(url=url, json=data).json()
+    return response["best_action"]
+
+
+act_1 = lambda observation, configuration: act(
+    "http://localhost:8081/actions/best_action", observation, configuration
+)
+act_2 = lambda observation, configuration: act(
+    "http://localhost:8082/actions/best_action", observation, configuration
+)
+
+# Replace `act_1` or `act_2` with "random" or "negamax" to play against default agents.
+env.run([act_2, act_1])
+env.render(mode="ipython", width=500, height=450)
