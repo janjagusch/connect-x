559b181d294c8ab973384c542faff28bda883224
jjagusch <jan@instaffo.de>
2020-04-05 16:49:07 +0200

    use player instead of mark

--- submission.py
+++ submission.py
@@ -16,11 +16,11 @@ from connect_x.utils.logger import setup_logger
 _LOGGER = setup_logger(__name__)
 
 
-def _catalogued_action(state):
-    return get_action(state)
+def _catalogued_action(state, player):
+    return get_action(state, player)
 
 
-def _planned_action(game, state):
+def _planned_action(game, state, player):
     return IterativeDeepening(
         negamax,
         timeout=game.timeout * TIMEOUT_BUFFER,
@@ -28,9 +28,9 @@ def _planned_action(game, state):
     )(
         game=game,
         state=state,
+        player=player,
         heuristic_func=heuristic,
         order_actions_func=order_actions,
-        player=state.mark - 1,
     )
 
 
@@ -46,13 +46,17 @@ def act(observation, configuration):
         int: The action.
     """
     start = datetime.now()
+
     game = ConnectXGame.from_configuration(configuration)
     state = ConnectXState.from_observation(
         observation, configuration.rows, configuration.columns
     )
+    player = observation.mark - 1
+
     _LOGGER.info(f"Round #{state.counter}!")
     _LOGGER.debug(f"State hash: '{state.state_hash}'")
-    action = _catalogued_action(state) or _planned_action(game, state)
+    _LOGGER.debug(f"Player: '{player}'.")
+    action = _catalogued_action(state, player) or _planned_action(game, state, player)
     end = datetime.now()
     _LOGGER.info(f"Action selected: '{action}'.")
     _LOGGER.debug(f"Time taken: {end - start}.")
