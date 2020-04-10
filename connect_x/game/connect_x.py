e5a88692652a9501d059020d92eef931aac83b45
H <haakon.robinson@gmail.com>
2020-04-10 23:52:44 +0200

    The configuration field names changed between versions of kaggle_environments. Added a check for this in the ConnectX game definition.

--- connect_x/game/connect_x.py
+++ connect_x/game/connect_x.py
@@ -1,10 +1,11 @@
 """
 This module defines the Connect-X game.
 """
-
+from distutils.version import StrictVersion as Version
 from copy import deepcopy
 
 import numpy as np
+from kaggle_environments import version as kaggle_env_version
 from kaggle_environments.utils import Struct
 
 from connect_x.game.game import Game, GameState
@@ -130,13 +131,23 @@ class ConnectXGame(Game):
         Returns:
             ConnectXGame: The game.
         """
-        return cls(
-            rows=configuration.rows,
-            columns=configuration.columns,
-            x=configuration.inarow,
-            timeout=configuration.timeout,
-            steps=configuration.steps,
-        )
+
+        if Version(kaggle_env_version) > Version("0.2.0"):
+            return cls(
+                rows=configuration.rows,
+                columns=configuration.columns,
+                x=configuration.inarow,
+                timeout=configuration.actTimeout,
+                steps=configuration.episodeSteps,
+            )
+        else:
+            return cls(
+                rows=configuration.rows,
+                columns=configuration.columns,
+                x=configuration.inarow,
+                timeout=configuration.timeout,
+                steps=configuration.steps,
+            )
 
     def to_configuration(self):
         """
